9a88a83526090fe713b4f95bcdaad1fc31ea6ea5
unknown <abdulbatinchy@gmail.com>
2023-12-25 11:28:47 +0600

    delete added

--- index.js
+++ index.js
@@ -38,6 +38,14 @@ async function run() {
       res.send(result)
     })
 
+    app.delete('/task/:id', async(req,res)=>{
+      const id = req.params.id;
+      console.log(id);
+      const query = {_id:new ObjectId(id)}
+      const result = await tasksCollections.deleteOne(query)
+      res.send(result)
+    })
+
 
 
 
